2816e92b3535405298a22696822851996a8c8e3f
misbahasim <39546951+misbahasim@users.noreply.github.com>
2021-08-22 16:31:24 +0500

    file uploaded

---
+++ asset/js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+    $("#menu").click(function () {
+        $(this).toggleClass("fa-bars fa-times");
+        $(".navbar").toggleClass("nav-toggle");
+    });
+
+    $(window).on("scroll load", function () {
+        $("#menu").removeClass("fa-times");
+        $("#menu").addClass("fa-bars");
+        $(".navbar").removeClass("nav-toggle");
+    });
+
+    $(".portfolio .button-container .btn").click(function () {
+        let filter = $(this).attr('data-filter');
+        if (filter == 'all') {
+            $(".portfolio .image-container .box").show("400");
+        }
+        else {
+            $(".portfolio .image-container .box").not('.' + filter).hide("200");
+            $(".portfolio .image-container .box").filter('.' + filter).show("200");
+        }
+    });
+
+    $('a[href*="#"]').on("click", function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top,
+        },
+            500,
+            'linear'
+        );
+    });
+
+});
